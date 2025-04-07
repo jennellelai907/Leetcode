970e87fd5a88af71ec7ddc6fc6f59f0ce3625843
jennellelai907 <Jennellelai907@users.noreply.github.com>
2025-04-07 22:58:32 +0800

    Update 13. Roman to Integer.py

--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -36,5 +36,20 @@ class Solution:
 
         return result
 
-
+# Optimization:
+# 使用字典來簡化羅馬數字的映射
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_values = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
+        result = 0
         
+        for i in range(len(s)):
+            # 如果當前數字小於下一個數字，則進行減法操作
+            if i < len(s) - 1 and roman_values[s[i]] < roman_values[s[i + 1]]:
+                result -= roman_values[s[i]]
+            else:
+                result += roman_values[s[i]]
+        
+        return result
